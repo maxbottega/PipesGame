7c2b4620d481d9866e2d7cdf9977253fdf7599c1
Jimbow <adeathjohn@gmail.com>
2014-02-16 01:15:45 +0800

    Add Timer

--- Assets/_PipeGame/script/Test.cs
+++ Assets/_PipeGame/script/Test.cs
@@ -1,15 +1,17 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class Test : MonoBehaviour {
 
 	// Use this for initialization
-	void Start () {
-	
-	}
-	
+	void Start () {		
+	}	
 	// Update is called once per frame
 	void Update () {
-	
+		Debub.Log("安安！");
+		if(this.isCool){
+			involke("so awesome !");
+		}
 	}
 }
+ // hheeeeee
